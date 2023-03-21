84dab3a5b21b93054771b5f71a78b6bd5ea3fc6c
omitogunjesufemi <omitogunjesufemi@gmail.com>
2023-03-21 14:37:05 +0000

    Entry point to the monty interpreter Added unistd, stdlib headers into header file

---
+++ main.c
@@ -0,0 +1,29 @@
+#include "monty.h"
+
+/**
+ * main - The entry point of the monty interpreter
+ * @argc: Argument count
+ * @argv: Arguments containing the file path
+ * Return: EXIT_SUCCESS or EXIT_FAILURE
+ */
+int main(int argc, char **argv)
+{
+	FILE *file;
+	char lineRead[1024];
+
+	if (argc < 2 || argc > 2)
+	{
+		fprintf(stderr, "USAGE: monty file\n");
+		exit(EXIT_FAILURE);
+	}
+
+	file = fopen(argv[1], "r");
+	if (file == NULL)
+	{
+		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+		exit(EXIT_FAILURE);
+	}
+
+	fclose(file);
+	return (EXIT_SUCCESS);
+}

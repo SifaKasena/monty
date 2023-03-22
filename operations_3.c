52e549e28b48a5be0aee21e020741ad4b9a7a7d6
omitogunjesufemi <omitogunjesufemi@gmail.com>
2023-03-22 15:11:37 +0000

    Moved function to operation_3.c

--- operations_3.c
+++ operations_3.c
@@ -25,3 +25,15 @@ void _queue(__attribute__((unused)) stack_t **stack,
 {
 	flag_stack = 1;
 }
+
+/**
+ * _revp - Prints linked list in reverse
+ * @stack: pointer to pointer to the head of the stack
+ * @line_number: line  number in the file
+ * Return: void
+ */
+
+void _revp(stack_t **stack, __attribute__((unused))unsigned int line_number)
+{
+	print_stack_reverse(*stack);
+}
